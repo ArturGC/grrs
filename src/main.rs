0180f27c1ac7dfe77dcc68a839cb322ff5929e76
arturgc <artur.garcia.costa@gmail.com>
2024-02-21 14:50:46 -0300

    done

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,15 @@
-use anyhow::{Context, Result};
 use clap::Parser;
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-};
 
 #[derive(Parser)]
-struct CLI {
+pub struct CLI {
     pattern: String,
     path: std::path::PathBuf,
 }
 
-struct Reader {
-    path: std::path::PathBuf,
-    reader: BufReader<File>,
-}
-impl Reader {
-    fn new(path: &std::path::PathBuf) -> Result<Reader, anyhow::Error> {
-        File::open(&path)
-            .map(|f| Reader {
-                path: path.clone(),
-                reader: BufReader::new(f),
-            })
-            .with_context(|| format!("could not read from file `{}`", path.display()))
-    }
-    fn get_line(&mut self) -> Result<String, anyhow::Error> {
-        let mut line = String::new();
+fn main() -> Result<(), anyhow::Error> {
+    use grrs::*;
 
-        self.reader
-            .read_line(&mut line)
-            .map(|_| line)
-            .with_context(|| format!("could not read line from file `{}`", self.path.display()))
-    }
-}
-
-struct Grep {
-    pattern: String,
-}
-impl Grep {
-    fn new(pattern: &String) -> Grep {
-        Grep {
-            pattern: pattern.clone(),
-        }
-    }
-    fn check(&self, line: String) {
-        let data = line.trim_end();
-
-        if data.contains(&self.pattern) {
-            println!("MATCH: {}", data);
-        } else {
-            println!("FAIL: {}", data);
-        };
-    }
-}
-
-fn main() -> Result<()> {
     let args = CLI::parse();
-
     let grep = Grep::new(&args.pattern);
     let mut reader = Reader::new(&args.path)?;
 
@@ -64,7 +17,7 @@ fn main() -> Result<()> {
         let line = reader.get_line()?;
 
         if line.len() != 0 {
-            grep.check(line);
+            grep.check(&line, &mut std::io::stdout());
         } else {
             break;
         };
